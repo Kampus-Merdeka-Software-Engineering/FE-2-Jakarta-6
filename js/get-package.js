d7d6552e653fd055edd4bfce0663233e258e284e
jeremyaryawanp <jeremyaryawanp@gmail.com>
2023-12-03 17:33:20 +0700

    Link to PackageById

---
+++ js/get-package.js
@@ -0,0 +1,37 @@
+// Packages Section
+    const linkDetailTidung = document.getElementById("pulauTidung")
+    linkDetailTidung.setAttribute("href", '/island.html?id=1')
+
+    const linkDetailBidadari = document.getElementById("pulauBidadari")
+    linkDetailBidadari.setAttribute("href", '/island.html?id=2')
+
+    const linkDetailPramuka = document.getElementById("pulauPramuka")
+    linkDetailPramuka.setAttribute("href", '/island.html?id=3')
+
+    const linkDetailPari = document.getElementById("pulauPari")
+    linkDetailPari.setAttribute("href", '/island.html?id=4')
+
+    const linkDetailOnrust = document.getElementById("pulauOnrust")
+    linkDetailOnrust.setAttribute("href", '/island.html?id=5')
+    
+    const linkDetailMacan = document.getElementById("pulauMacan")
+    linkDetailMacan.setAttribute("href", '/island.html?id=6')
+
+// Gallery Section
+    const linkGalleryTidung = document.getElementById("galleryTidung")
+    linkGalleryTidung.setAttribute("href", '/island.html?id=1')
+
+    const linkGalleryBidadari = document.getElementById("galleryBidadari")
+    linkGalleryBidadari.setAttribute("href", '/island.html?id=2')
+
+    const linkGalleryPramuka = document.getElementById("galleryPramuka")
+    linkGalleryPramuka.setAttribute("href", '/island.html?id=3')
+
+    const linkGalleryPari = document.getElementById("galleryPari")
+    linkGalleryPari.setAttribute("href", '/island.html?id=4')
+
+    const linkGalleryOnrust = document.getElementById("galleryOnrust")
+    linkGalleryOnrust.setAttribute("href", '/island.html?id=5')
+
+    const linkGalleryMacan = document.getElementById("galleryMacan")
+    linkGalleryMacan.setAttribute("href", '/island.html?id=6')
